57fd1ca9df144966d683b7aec7f6ca42795acb1f
DragFAQ <drag.faq@gmail.com>
2019-07-10 00:51:55 +0300

    Summary fixed

--- src/summary.ts
+++ src/summary.ts
@@ -1,19 +1,17 @@
 import { registerMethod } from "./utils";
+import { IPath } from "./index";
 
 export const TAG_SUMMARY = Symbol("Summary");
 
 const SUMMARIES: Map<Function, Map<string, string>> = new Map();
 
-export function summary(summary: string): MethodDecorator {
-  return function (target: any, key: string) {
-    if (!SUMMARIES.has(target.constructor)) {
-      SUMMARIES.set(target.constructor, new Map());
-    }
-    SUMMARIES.get(target.constructor).set(key, summary);
-    registerMethod(target, key, function fnSummary(router) {
-      router.summary = summary;
-    });
-    target[TAG_SUMMARY] = target.constructor[TAG_SUMMARY] = SUMMARIES.get(target.constructor);
+export const summary = (summary: string): MethodDecorator => (target: any, key: string): void => {
+  if (!SUMMARIES.has(target.constructor)) {
+    SUMMARIES.set(target.constructor, new Map());
   }
-}
-
+  SUMMARIES.get(target.constructor).set(key, summary);
+  registerMethod(target, key, (router: IPath): void => {
+    router.summary = summary;
+  });
+  target[TAG_SUMMARY] = target.constructor[TAG_SUMMARY] = SUMMARIES.get(target.constructor);
+};
