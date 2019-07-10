931d880e26e1ac0676603870d7a68f7cf013575c
DragFAQ <drag.faq@gmail.com>
2019-07-10 11:25:04 +0300

    fixed tag and response

--- src/response.ts
+++ src/response.ts
@@ -1,7 +1,8 @@
 import { ISchema, toJoi, toSwagger } from "./ischema";
 import * as joi from "joi";
 import { registerMethod, registerMiddleware } from "./utils";
-import { HTTPStatusCodes } from "./index";
+import { HTTPStatusCodes, IPath } from "./index";
+import { BaseContext } from "koa";
 
 export const TAG_RESPONSE = Symbol("Response");
 
@@ -9,44 +10,42 @@ const RESPONSES: Map<Function, Map<string, Map<number, ISchema | joi.Schema>>> =
 
 export const DEFAULT_RESPONSE: joi.Schema = joi.string().default("");
 
-export function response(code: number, schema?: ISchema | joi.Schema): MethodDecorator {
-  return function (target: any, key: string) {
-    if (!schema) {
-      schema = DEFAULT_RESPONSE;
-    }
-    if (!RESPONSES.has(target.constructor)) {
-      RESPONSES.set(target.constructor, new Map());
-    }
-    if (!RESPONSES.get(target.constructor).has(key)) {
-      RESPONSES.get(target.constructor).set(key, new Map());
-    }
-    registerMethod(target, key, function fnResponse(router) {
-      if (!router.responses) {
-        router.responses = {};
-      }
-      schema = toSwagger(schema);
-      let description = "";
-      if (schema["description"]) {
-        description = schema["description"];
-        delete schema["description"];
-      }
-      router.responses[code] = Object.assign({description: description}, {schema});
-    });
-
-    registerMiddleware(target, key, async function fnResponse(ctx, next) {
-      await next();
-      if (RESPONSES.get(target.constructor).get(key).has(ctx.status)) {
-        let {error, value} = joi.validate(ctx.body, RESPONSES.get(target.constructor).get(key).get(ctx.status));
-        if (error) {
-          ctx.body = {code: HTTPStatusCodes.internalServerError, message: error.message};
-          ctx.status = HTTPStatusCodes.internalServerError;
-          return;
-        }
-        ctx.body = value;
-      }
-    });
-
-    RESPONSES.get(target.constructor).get(key).set(code, toJoi(schema));
-    target[TAG_RESPONSE] = target.constructor[TAG_RESPONSE] = RESPONSES.get(target.constructor);
+export const response = (code: number, schema?: ISchema | joi.Schema): MethodDecorator => (target: {}, key: string): void => {
+  if (!schema) {
+    schema = DEFAULT_RESPONSE;
   }
-}
+  if (!RESPONSES.has(target.constructor)) {
+    RESPONSES.set(target.constructor, new Map());
+  }
+  if (!RESPONSES.get(target.constructor).has(key)) {
+    RESPONSES.get(target.constructor).set(key, new Map());
+  }
+  registerMethod(target, key, (router: IPath): void => {
+    if (!router.responses) {
+      router.responses = {};
+    }
+    schema = toSwagger(schema);
+    let description = "";
+    if (schema["description"]) {
+      description = schema["description"];
+      delete schema["description"];
+    }
+    router.responses[code] = Object.assign({description: description}, {schema});
+  });
+
+  registerMiddleware(target, key, async (ctx: BaseContext, next: () => {}): Promise<void> => {
+    await next();
+    if (RESPONSES.get(target.constructor).get(key).has(ctx.status)) {
+      const {error, value} = joi.validate(ctx.body, RESPONSES.get(target.constructor).get(key).get(ctx.status));
+      if (error) {
+        ctx.body = {code: HTTPStatusCodes.internalServerError, message: error.message};
+        ctx.status = HTTPStatusCodes.internalServerError;
+        return;
+      }
+      ctx.body = value;
+    }
+  });
+
+  RESPONSES.get(target.constructor).get(key).set(code, toJoi(schema));
+  target[TAG_RESPONSE] = target.constructor[TAG_RESPONSE] = RESPONSES.get(target.constructor);
+};
--- src/tag.ts
+++ src/tag.ts
@@ -1,24 +1,23 @@
 import { registerMethod } from "./utils";
+import { IPath } from "./index";
 
 export const TAG_TAG = Symbol("Tag");
 
 const TAGS: Map<Function, Map<string, Set<string>>> = new Map();
 
-export function tag(tag: string): MethodDecorator {
-  return function (target: any, key: string) {
-    if (!TAGS.has(target.constructor)) {
-      TAGS.set(target.constructor, new Map());
-    }
-    if (!TAGS.get(target.constructor).has(key)) {
-      TAGS.get(target.constructor).set(key, new Set());
-    }
-    registerMethod(target, key, function fnTag(router) {
-      if (!router.tags) {
-        router.tags = [];
-      }
-      router.tags.push(tag);
-    });
-    TAGS.get(target.constructor).get(key).add(tag);
-    target[TAG_TAG] = target.constructor[TAG_TAG] = TAGS.get(target.constructor);
+export const tag = (tag: string): MethodDecorator => (target: {}, key: string) => {
+  if (!TAGS.has(target.constructor)) {
+    TAGS.set(target.constructor, new Map());
   }
-}
+  if (!TAGS.get(target.constructor).has(key)) {
+    TAGS.get(target.constructor).set(key, new Set());
+  }
+  registerMethod(target, key, (router: IPath): void => {
+    if (!router.tags) {
+      router.tags = [];
+    }
+    router.tags.push(tag);
+  });
+  TAGS.get(target.constructor).get(key).add(tag);
+  target[TAG_TAG] = target.constructor[TAG_TAG] = TAGS.get(target.constructor);
+};
