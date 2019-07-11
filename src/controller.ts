244abe26e0d9f4954d24447e5d1b8ff12c5e355e
DragFAQ <drag.faq@gmail.com>
2019-07-11 15:45:04 +0300

    TSLint fixes

--- src/controller.ts
+++ src/controller.ts
@@ -2,16 +2,16 @@ export const TAG_CONTROLLER = Symbol("Controller");
 
 /**
  * Controller
- * @param path
- * @returns {(Controller:Function)=>undefined}
+ * @param path relative path
+ * @returns ClassDecorator {(Controller:Function)=>undefined}
  */
 export const controller = (path?: string): ClassDecorator => (Controller: Function): void => {
   if (!path) {
     path = Controller.name;
   }
   const parent = Object.getPrototypeOf(Controller);
-  if (parent[TAG_CONTROLLER]) {
-    path = parent[TAG_CONTROLLER] + path;
+  if (parent[Symbol.for("Controller")]) {
+    path = parent[Symbol.for("Controller")] + path;
   }
-  Controller[TAG_CONTROLLER] = path;
+  Controller[Symbol.for("Controller")] = path;
 };
